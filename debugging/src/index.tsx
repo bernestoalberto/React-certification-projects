bf35a72975058d575335a8e475e768d646b5801b
Ernesto Bonet Moncada <bernestoalberto@gmail.com>
2025-04-08 07:01:48 -0400

    debbuger project updated

--- debugging/src/index.tsx
+++ debugging/src/index.tsx
@@ -5,8 +5,8 @@ import { StrictMode } from 'react';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode >  
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode >
     <App />
-</ StrictMode >
+  </ StrictMode >
 );
